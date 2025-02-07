956d4d54c9ee67e735a6196098eff25b4f7c7a05
balu <balasrinivas6789@gmail.com>
2025-02-07 15:25:36 +0530

    headerbranch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-     <h1 className="bg-black text-white text-[100px]">hello ramnayak</h1>
+     {/* <h1 className="bg-black text-white text-[100px]">hello world</h1> */}
+     <Header />
     </div>
   );
 }
